ee7ee3a90fd7d5a78bca0115a2c7ec0968304d88
Braxton Bobo <Braxton_bobo@baylor.edu>
2021-03-29 18:21:37 -0500

    Final Submit

--- NLP_HW_Assignment.py
+++ NLP_HW_Assignment.py
@@ -1,3 +1,4 @@
+# import things
 from textblob import TextBlob
 from pathlib import Path
 import nltk
@@ -6,13 +7,11 @@ from wordcloud import WordCloud
 import pandas as pd
 from textblob import Word
 from operator import itemgetter
-from nltk.stem import PorterStemmer
-from nltk.stem import WordNetLemmatizer
-
-porter = PorterStemmer()
-wnl = WordNetLemmatizer()
-
+ 
+# Create object to hold stop words
 stops = stopwords.words("english")
+ 
+# create a list that contains the book of john stop words
 Book_of_John_Stop_Words = [
     "thy",
     "ye",
@@ -25,82 +24,57 @@ Book_of_John_Stop_Words = [
     "shall",
     "saith",
 ]
-
+ 
+# add in the book of john stop words
 stops += Book_of_John_Stop_Words
-
-sorted_stops = sorted(stops)
-
-print(sorted_stops)
-
+ 
+# read the book of john text
 text = Path("book of John text.txt").read_text()
-
+ 
+# Tokenize the words
 tokens = nltk.word_tokenize(text)
-
+ 
+# Create a list of tuples that contain the word and its tag
 tags = nltk.pos_tag(tokens)
-
-# print(tags)
-
+ 
+# Filter out nouns
 nouns = [
     word
     for word, pos in tags
-    if pos == "NN" or pos == "NNP" or pos == "NNS" or pos == "NNPS"
+    if (pos == "NN" or pos == "NNP" or pos == "NNS" or pos == "NNPS")
 ]
-
-# print(nouns)
-
+ 
+# Convert to text of nouns
 noun_text = " ".join(nouns).lower()
-
+ 
+# Turn noun text into a blob
 noun_blob = TextBlob(noun_text)
-
-##
-# print(noun_blob)
-
-# Correct to this point 9:21A
-
-# list_of_nouns = []
-
-# for word in noun_blob.words:
-#     list_of_nouns.append(word)
-
-# Formatted_nouns = []
-# for word in list_of_nouns:
-#     porter.stem(word)
-#     wnl.lemmatize(word)
-#     Formatted_nouns.append(word)
-
-# formatted_text = " ".join(Formatted_nouns)
-
-# formatted_text_blob = TextBlob(formatted_text)
-
+ 
+# Count the items in the blob
 items = noun_blob.word_counts.items()
-
-# print(items)
-
-# Jesus,247 apperas on this lone
-
+ 
+# Filter out the stops
 items_no_stops = [item for item in items if item[0] not in stops]
-
-# print(items_no_stops)
-
-# Jesus,247 appears
-
-sorted_items = sorted(items, key=itemgetter(1), reverse=True)
-
-# print(sorted_items)
-
-top15 = sorted_items[0:15]
-
-print(top15)
-
+ 
+# Sort the items by their count
+sorted_items = sorted(items_no_stops, key=itemgetter(1), reverse=True)
+ 
+# Store top 15 in top15 variable
+top15 = sorted_items[:15]
+ 
+# Create a list to hold the words
 words123 = []
-
+ 
+# Append into words123 just the words
 for x, y in top15:
     words123.append(x)
-
+ 
+# Turn the words into text
 text123 = " ".join(words123)
-
+ 
+# generate wordcloud, send it to the bookofjohnwordcloud file
 wordcloud = WordCloud(colormap="prism", background_color="white")
-
+ 
 wordcloud = wordcloud.generate(text123)
-
+ 
 wordcloud = wordcloud.to_file("BookOfJohnWordCloud.png")
